47880a76a402f34d7c8438b07a84c730d9abc55a
Robin Hermans <r.hermans@maxxton.com>
2015-04-07 16:29:53 +0200

    + added first version of the DataContainer class * changed Message class to support set comparison

--- src/main/java/com/maxxton/aam/communication/DataContainer.java
+++ src/main/java/com/maxxton/aam/communication/DataContainer.java
@@ -1,8 +1,13 @@
 package com.maxxton.aam.communication;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+import com.maxxton.aam.messages.Message;
+
 /**
- * DataContainer class
- * Contains all data which passes the Send- and/or ReceiveController.
+ * DataContainer class Contains all data which passes the Send- and/or ReceiveController.
  * This may include id's and messages.
  * 
  * @author Robin Hermans
@@ -10,5 +15,216 @@ package com.maxxton.aam.communication;
  */
 public class DataContainer
 {
+  private static DataContainer instance = null;
 
+  private Set<String> ssIdentifiers;
+  private Set<Message> smSendMessages;
+  private Set<Message> smReceivedMessages;
+
+  /**
+   * DataContainer constructor Initiates elements defined in this class
+   */
+  public DataContainer()
+  {
+    this.ssIdentifiers = new HashSet<String>();
+    this.smSendMessages = new HashSet<Message>();
+    this.smReceivedMessages = new HashSet<Message>();
+  }
+
+  /**
+   * Singleton getInstance class. Creates a new or return an existing instance of this class.
+   * 
+   * @return an instance of the DataContainer class
+   */
+  public static DataContainer getInstance()
+  {
+    if (instance == null)
+    {
+      instance = new DataContainer();
+    }
+    return instance;
+  }
+
+  /**
+   * Gets an unique identifier
+   * 
+   * @return an unique id given as string
+   */
+  public String getUniqueId()
+  {
+    String id = UUID.randomUUID().toString();
+    this.ssIdentifiers.add(id);
+    return id;
+  }
+
+  /**
+   * Check whether or not a message was created and send from this client.
+   * 
+   * @param id
+   *          identifier of a message
+   * @return true if owned, false if not.
+   */
+  public boolean isOwnedByMe(String id)
+  {
+    if (this.ssIdentifiers.contains(id))
+    {
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * Removes a given messagid from the list if it exists.
+   * 
+   * @param id
+   *          identifier of a message
+   */
+  public void removeId(String id)
+  {
+    if (this.ssIdentifiers.contains(id))
+    {
+      this.ssIdentifiers.remove(id);
+    }
+  }
+
+  /**
+   * Getter for HashSet with string bases identifiers.
+   * 
+   * @return an Set of identifiers.
+   */
+  public Set<String> getIdentifiers()
+  {
+    return this.ssIdentifiers;
+  }
+
+  /**
+   * Setter for Set with string bases identifiers.
+   * 
+   * @param identifiers
+   *          an Set of string identifiers.
+   */
+  public void setIdentifiers(Set<String> identifiers)
+  {
+    this.ssIdentifiers = identifiers;
+  }
+  
+  /**
+   * Adds a given message to the send Set.
+   * 
+   * @param message
+   *          the Message object.
+   */
+  public void addSendMessage(Message message)
+  {
+    this.smSendMessages.add(message);
+  }
+
+  /**
+   * Removes a given message from the send Set.
+   * 
+   * @param message
+   *          the Message object.
+   */
+  public void removeSendMessage(Message message)
+  {
+    this.smSendMessages.remove(message);
+  }
+
+  /**
+   * Removes a given message from the send Set.
+   * 
+   * @param id
+   *          identifier of the message
+   */
+  public void removeSendMessageById(String id)
+  {
+    for (Message message : this.smSendMessages)
+    {
+      if (message.getMessageId().equals(id))
+      {
+        this.smSendMessages.remove(message);
+      }
+    }
+  }
+
+  /**
+   * Setter for the messages send Set.
+   * 
+   * @param messages
+   *          the Set with Message objects.
+   */
+  public void setSendMessages(Set<Message> messages)
+  {
+    this.smReceivedMessages = messages;
+  }
+
+  /**
+   * Getter for the messages send Set.
+   * 
+   * @return Set with messages.
+   */
+  public Set<Message> getSendMessages()
+  {
+    return this.smReceivedMessages;
+  }
+
+  /**
+   * Adds a given message to the receive Set.
+   * 
+   * @param message
+   *          the Message object.
+   */
+  public void addReceivedMessage(Message message)
+  {
+    this.smReceivedMessages.add(message);
+  }
+
+  /**
+   * Removes a given message from the receive Set.
+   * 
+   * @param message
+   *          the Message object.
+   */
+  public void removeReceivedMessage(Message message)
+  {
+    this.smReceivedMessages.remove(message);
+  }
+
+  /**
+   * Removes a given message from the received Set.
+   * 
+   * @param id
+   *          identifier of the message
+   */
+  public void removeReceivedMessageById(String id)
+  {
+    for (Message message : this.smReceivedMessages)
+    {
+      if (message.getMessageId().equals(id))
+      {
+        this.smSendMessages.remove(message);
+      }
+    }
+  }
+
+  /**
+   * Setter for the messages receive Set.
+   * 
+   * @param messages
+   *          the Set with Message objects.
+   */
+  public void setReceivedMessages(Set<Message> messages)
+  {
+    this.smReceivedMessages = messages;
+  }
+
+  /**
+   * Getter for the messages receive Set.
+   * 
+   * @return Set with messages.
+   */
+  public Set<Message> getReceivedMessages()
+  {
+    return this.smReceivedMessages;
+  }
 }
--- src/main/java/com/maxxton/aam/messages/Message.java
+++ src/main/java/com/maxxton/aam/messages/Message.java
@@ -1,14 +1,81 @@
 package com.maxxton.aam.messages;
 
 /**
- * Message interface
- * Skeleton class for the different types of messages.
+ * Message interface Skeleton class for the different types of messages. 
  * Includes the mandatory fields to be used by it's extender.
  * 
  * @author Robin Hermans
  * @copyright Maxxton 2015
  */
-public interface Message
+
+public abstract class Message
 {
+  protected String strMessageId;
+
+  protected MessageType mtMessageType;
+
+  /**
+   * Sets the identifier of this message
+   * 
+   * @param messageId
+   */
+  public void setMessageId(String messageId)
+  {
+    this.strMessageId = messageId;
+  }
+
+  /**
+   * Gets the identifier of this message
+   * 
+   * @return message identifier as string
+   */
+  public String getMessageId()
+  {
+    return this.strMessageId;
+  }
+
+  /**
+   * Generates a hashcode from two class related variables.
+   * 
+   * @return hashcode result of the two variables as integer
+   */
+  @Override
+  public int hashCode()
+  {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((mtMessageType == null) ? 0 : mtMessageType.hashCode());
+    result = prime * result + ((strMessageId == null) ? 0 : strMessageId.hashCode());
+    return result;
+  }
+
+  /**
+   * Method used by a set to check for duplicates.
+   * 
+   * @param obj
+   *          object to be compared against the current one
+   * @return true if the object matches the criteria, false if it doesn't
+   */
+  @Override
+  public boolean equals(Object obj)
+  {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (!(obj instanceof Message))
+      return false;
+    Message other = (Message) obj;
+    if (mtMessageType != other.mtMessageType)
+      return false;
+    if (strMessageId == null)
+    {
+      if (other.strMessageId != null)
+        return false;
+    }
+    else if (!strMessageId.equals(other.strMessageId))
+      return false;
+    return true;
+  }
 
 }
