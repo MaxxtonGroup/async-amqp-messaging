a53069a1e0c621212b78cb39a9e4644f7b17e9ed
Robin Hermans <r.hermans@maxxton.com>
2015-04-17 09:13:08 +0200

    * Fixed issues for ReceiveController class tests.

--- src/test/java/com/maxxton/test/communication/ReceiveControllerTests.java
+++ src/test/java/com/maxxton/test/communication/ReceiveControllerTests.java
@@ -1,21 +1,23 @@
 package com.maxxton.test.communication;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageProperties;
 
+import com.maxxton.aam.communication.DataContainer;
 import com.maxxton.aam.communication.MessageSerializer;
 import com.maxxton.aam.communication.ReceiveController;
 import com.maxxton.aam.communication.SendController;
 import com.maxxton.aam.messages.BaseMessage;
 import com.maxxton.aam.messages.GenerateMessage;
+import com.maxxton.aam.resources.Callback;
 import com.maxxton.aam.resources.Resources;
 
 /**
@@ -75,86 +77,86 @@ public class ReceiveControllerTests
     System.out.println("done.");
   }
 
-  // /**
-  // * Test the getter and setter for the datacontainer class.
-  // *
-  // * @throws Exception
-  // * reason of failure given by the test.
-  // */
-  // @Test
-  // public void testDataContainer() throws Exception
-  // {
-  // System.out.print("ReceiveController : Testing getter and setter for the datacontainer instance...");
-  //
-  // DataContainer testContainer = this.objReceiver.getDataContainer();
-  // assertNotNull("The datacontainer instance cannot be null.", testContainer);
-  //
-  // DataContainer otherContainer = DataContainer.getInstance("other");
-  // this.objReceiver.setDataContainer(otherContainer);
-  //
-  // DataContainer sameContainer = this.objReceiver.getDataContainer();
-  // assertNotNull("The datacontainer instance cannot be null.", sameContainer);
-  //
-  // assertNotEquals("The datacontainer instances should not be te same.", testContainer, sameContainer);
-  // assertEquals("The datacontainer instances should be the same.", otherContainer, sameContainer);
-  //
-  // System.out.println("done.");
-  // }
-  //
-  // /**
-  // * Test the getter and setter for the resources class.
-  // *
-  // * @throws Exception
-  // * reason of failure given by the test.
-  // */
-  // @Test
-  // public void testResources() throws Exception
-  // {
-  // System.out.print("ReceiveController : Testing getter and setter for the resources instance...");
-  //
-  // Resources testResources = this.objReceiver.getResources();
-  // assertNotNull("The resources instance cannot be null.", testResources);
-  //
-  // Resources otherResources = new Resources();
-  // this.objReceiver.setResources(otherResources);
-  //
-  // Resources sameResources = this.objReceiver.getResources();
-  // assertNotNull("The resources instance cannot be null.", sameResources);
-  //
-  // assertNotEquals("The resource instances should not be the same", testResources, sameResources);
-  // assertEquals("The resource instances should be the same.", otherResources, sameResources);
-  //
-  // System.out.println("done.");
-  // }
-  //
-  // /**
-  // * Test the getter and setter for the callback class.
-  // *
-  // * @throws Exception
-  // * reason of failure given by the test.
-  // */
-  // @Test
-  // public void testCallback() throws Exception
-  // {
-  // System.out.print("ReceiveController : Testing getter and setter for the callback instance...");
-  //
-  // // TODO : Add test for receiving a message though the callback.
-  // Callback testCallback = this.objReceiver.getCallback();
-  // assertNull("The callback instance should be null.", testCallback);
-  //
-  // Callback otherCallback = new Callback();
-  // this.objReceiver.setCallback(otherCallback);
-  //
-  // Callback sameCallback = this.objReceiver.getCallback();
-  // assertNotNull("The callback instance cannot be null.", sameCallback);
-  //
-  // assertNotEquals("The callback instances cannot be the same.", testCallback, sameCallback);
-  // assertEquals("The callback instances should be the same.", otherCallback, sameCallback);
-  //
-  // this.objReceiver.setCallback(testCallback);
-  //
-  // System.out.println("done.");
-  // }
+  /**
+   * Test the getter and setter for the datacontainer class.
+   *
+   * @throws Exception
+   *           reason of failure given by the test.
+   */
+  @Test
+  public void testDataContainer() throws Exception
+  {
+    System.out.print("ReceiveController : Testing getter and setter for the datacontainer instance...");
+
+    DataContainer testContainer = this.objReceiver.getDataContainer();
+    assertNotNull("The datacontainer instance cannot be null.", testContainer);
+
+    DataContainer otherContainer = DataContainer.getInstance("other");
+    this.objReceiver.setDataContainer(otherContainer);
+
+    DataContainer sameContainer = this.objReceiver.getDataContainer();
+    assertNotNull("The datacontainer instance cannot be null.", sameContainer);
+
+    assertNotEquals("The datacontainer instances should not be te same.", testContainer, sameContainer);
+    assertEquals("The datacontainer instances should be the same.", otherContainer, sameContainer);
+
+    System.out.println("done.");
+  }
+
+  /**
+   * Test the getter and setter for the resources class.
+   *
+   * @throws Exception
+   *           reason of failure given by the test.
+   */
+  @Test
+  public void testResources() throws Exception
+  {
+    System.out.print("ReceiveController : Testing getter and setter for the resources instance...");
+
+    Resources testResources = this.objReceiver.getResources();
+    assertNotNull("The resources instance cannot be null.", testResources);
+
+    Resources otherResources = new Resources();
+    this.objReceiver.setResources(otherResources);
+
+    Resources sameResources = this.objReceiver.getResources();
+    assertNotNull("The resources instance cannot be null.", sameResources);
+
+    assertNotEquals("The resource instances should not be the same", testResources, sameResources);
+    assertEquals("The resource instances should be the same.", otherResources, sameResources);
+
+    System.out.println("done.");
+  }
+
+  /**
+   * Test the getter and setter for the callback class.
+   *
+   * @throws Exception
+   *           reason of failure given by the test.
+   */
+  @Test
+  public void testCallback() throws Exception
+  {
+    System.out.print("ReceiveController : Testing getter and setter for the callback instance...");
+
+    // TODO : Add test for receiving a message though the callback.
+    Callback testCallback = this.objReceiver.getCallback();
+    assertNull("The callback instance should be null.", testCallback);
+
+    Callback otherCallback = new Callback();
+    this.objReceiver.setCallback(otherCallback);
+
+    Callback sameCallback = this.objReceiver.getCallback();
+    assertNotNull("The callback instance cannot be null.", sameCallback);
+
+    assertNotEquals("The callback instances cannot be the same.", testCallback, sameCallback);
+    assertEquals("The callback instances should be the same.", otherCallback, sameCallback);
+
+    this.objReceiver.setCallback(testCallback);
+
+    System.out.println("done.");
+  }
 
   /**
    * Cleanup method called after running the test to cleanup.
