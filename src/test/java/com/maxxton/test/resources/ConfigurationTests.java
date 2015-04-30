a0e80a73a5a6d21144d3ee5fa7c5af80fcc41593
Robin Hermans <r.hermans@maxxton.com>
2015-04-30 15:11:52 +0200

    + Added an unit test for the Configuration class.

---
+++ src/test/java/com/maxxton/test/resources/ConfigurationTests.java
@@ -0,0 +1,99 @@
+package com.maxxton.test.resources;
+
+import java.util.ArrayList;
+
+import org.junit.After;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import com.maxxton.aam.resources.Configuration;
+
+/**
+ * Runs a list of tests against the Configuration class.
+ * 
+ * @author Robin Hermans
+ * @copyright Maxxton 2015
+ */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class ConfigurationTests
+{
+  private Configuration objConfiguration;
+
+  @Before
+  public void setup()
+  {
+    if (this.objConfiguration == null)
+    {
+      this.objConfiguration = new Configuration();
+      assertNotNull("The configuration object cannot be null.", this.objConfiguration);
+    }
+  }
+
+  /**
+   * Test loading the default properties through the constructor.
+   * 
+   * @throws Exception
+   *           reason of failure given by the test.
+   */
+  @Test
+  public void testDefaultLoading() throws Exception
+  {
+    System.out.print("Configuration : Testing default configuration loading...");
+
+    String strHost = this.objConfiguration.getHost();
+    assertEquals("The host should equal 'localhost'.", strHost, "localhost");
+
+    ArrayList<Integer> arrPorts = this.objConfiguration.getPorts();
+    int iPort = arrPorts.get(0);
+    assertEquals("The ports should equal '5672'.", iPort, 5672);
+
+    String strUsername = this.objConfiguration.getUsername();
+    assertEquals("The username should equal 'username'.", strUsername, "guest");
+
+    String strPassword = this.objConfiguration.getPassword();
+    assertEquals("The password should equal 'username'.", strPassword, "guest");
+
+    System.out.println("done.");
+  }
+
+  /**
+   * Tests loading a custom properties file.
+   * 
+   * @throws Exception
+   *           reason of failure given by the test.
+   */
+  @Test
+  public void testConfigurationLoading() throws Exception
+  {
+    System.out.print("Configuration : Testing custom configuration loading...");
+
+    this.objConfiguration.loadConfiguration("/test.properties");
+
+    String strHost = this.objConfiguration.getHost();
+    assertEquals("The host should equal '192.168.252.141'.", strHost, "192.168.252.141");
+
+    ArrayList<Integer> arrPorts = this.objConfiguration.getPorts();
+    int iPort = arrPorts.get(0);
+    assertEquals("The ports should equal '5672'.", iPort, 5672);
+
+    String strUsername = this.objConfiguration.getUsername();
+    assertEquals("The username should equal 'username'.", strUsername, "username");
+
+    String strPassword = this.objConfiguration.getPassword();
+    assertEquals("The password should equal 'username'.", strPassword, "password");
+
+    System.out.println("done.");
+  }
+
+  @After
+  public void cleanup()
+  {
+
+  }
+
+}
--- src/test/java/com/maxxton/test/resources/MessageDetailsTests.java
+++ src/test/java/com/maxxton/test/resources/MessageDetailsTests.java
@@ -37,12 +37,16 @@ public class MessageDetailsTests
   @Test
   public void testResponseId() throws Exception
   {
+    System.out.print("MessageDetails : Testing the Response Id getter and setter...");
+
     String strResponseId = "123456789";
     MessageDetails objDetails = new MessageDetails(strResponseId, null, null, null, null);
     assertNotNull("The MessageDetails instance cannot be null", objDetails);
 
     String strSameResponseId = objDetails.getResponseId();
     assertEquals("The response id's should be the same.", strResponseId, strSameResponseId);
+
+    System.out.println("done.");
   }
 
   /**
@@ -54,12 +58,16 @@ public class MessageDetailsTests
   @Test
   public void testSender() throws Exception
   {
+    System.out.print("MessageDetails : Testing the Sender getter and setter...");
+
     String strSender = "testSender";
     MessageDetails objDetails = new MessageDetails(null, strSender, null, null, null);
     assertNotNull("The MessageDetails instance cannot be null", objDetails);
 
     String strSameSender = objDetails.getSender();
     assertEquals("The senders should be the same.", strSender, strSameSender);
+
+    System.out.println("done.");
   }
 
   /**
@@ -71,12 +79,16 @@ public class MessageDetailsTests
   @Test
   public void testReceiver() throws Exception
   {
+    System.out.print("MessageDetails : Testing the Receiver getter and setter...");
+
     String strReceiver = "testReceiver";
     MessageDetails objDetails = new MessageDetails(null, null, strReceiver, null, null);
     assertNotNull("The MessageDetails instance cannot be null", objDetails);
 
     String strSameReceiver = objDetails.getReceiver();
     assertEquals("The receivers should be the same.", strReceiver, strSameReceiver);
+
+    System.out.println("done.");
   }
 
   /**
@@ -88,12 +100,16 @@ public class MessageDetailsTests
   @Test
   public void testMessageType() throws Exception
   {
+    System.out.print("MessageDetails : Testing the MessageType getter and setter...");
+
     MessageType objType = MessageType.GENERATION_MESSAGE;
     MessageDetails objDetails = new MessageDetails(null, null, null, objType, null);
     assertNotNull("The MessageDetails instance cannot be null", objDetails);
 
     MessageType strSameType = objDetails.getMessageType();
     assertEquals("The message types should be the same.", objType, strSameType);
+
+    System.out.println("done.");
   }
 
   /**
@@ -105,12 +121,16 @@ public class MessageDetailsTests
   @Test
   public void testPayload() throws Exception
   {
+    System.out.print("MessageDetails : Testing the Payload getter and setter...");
+
     Object objMessage = "Hello World";
     MessageDetails objDetails = new MessageDetails(null, null, null, null, objMessage);
     assertNotNull("The MessageDetails instance cannot be null", objDetails);
 
     Object strSameMessage = objDetails.getPayload();
     assertEquals("The payload should be the same.", objMessage, strSameMessage);
+
+    System.out.println("done.");
   }
 
   @After
